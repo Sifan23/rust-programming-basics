e4c699f2362dfcc697038a8490755a90c0324b18
Sifan23 <sifanyg123@gmail.com>
2025-08-19 18:16:51 +0300

    feat: hello component

---
+++ examples/hello.rs
@@ -0,0 +1,9 @@
+#![allow(unused)]
+// main() is the entry point of a Rust program
+
+// Macros in Rust generate code at compile time and are
+// invoked with an exclamation mark (!).
+
+fn main(){
+    println!("Hello, world");
+}
