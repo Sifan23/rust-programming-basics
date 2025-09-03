7d37c7fed80708b7c96fbd8170b77b4661c182af
Sifan23 <sifanyg123@gmail.com>
2025-09-03 13:41:31 +0300

    feat: fn exercises

---
+++ examples/function.rs
@@ -0,0 +1,29 @@
+#![allow(unused)]
+
+fn add_with_return(x: u32, y: u32) -> u32 {
+    return x + y ;
+}
+
+fn add (x: u32, y: u32) -> u32 {
+   
+    x + y 
+}
+
+fn mul (x: u32, y: u32) -> u32 {
+    x * y
+}
+
+fn div (x: u32, y: u32) -> u32 {
+    x / y
+}
+
+fn main () {
+     let x = 2;
+     let y = 3;
+     let z = add(x, y);
+        println!("The sum of {} and {} is {}", x, y, z);
+    let z = mul(x,y);
+        println!("The multiplication of {} and {} is {}", x,y,z);
+    let z = div(x,y);
+        println!("The division of {} and {} is {}", x,y,z);
+}
