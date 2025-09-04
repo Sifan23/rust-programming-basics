143d9619e06afc3d15954851aa52f770a58f55bc
Sifan23 <sifanyg123@gmail.com>
2025-09-04 09:32:34 +0300

    feat: char comparison excercise

---
+++ examples/scalar_exercise.rs
@@ -0,0 +1,18 @@
+pub fn eq() {
+    let x: char = 'a';
+    let y: char = 'b';
+    let z: bool = x == y;
+    println!("{x} == {y} is {z}");
+}
+
+// pub fn add() {
+//     todo!();
+// }
+
+// pub fn cast(x: u8, y: i8, z: f32) -> f32 {
+//     x + y + z
+// }
+
+pub fn main() {
+    eq();
+}
